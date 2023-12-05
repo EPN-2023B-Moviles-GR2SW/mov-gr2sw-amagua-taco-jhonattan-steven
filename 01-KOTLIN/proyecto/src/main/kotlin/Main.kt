aefa2d2e8002aad96440e5299b2119abb9804549
Jhonattan Amagua <jhonattan.amagua@epn.edu.ec>
2023-12-05 05:49:35 -0500

    Control

--- 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -92,6 +92,7 @@ class Suma( // Constructor Primario Suma
         this.numeroUno
         this.numeroDos
     }
+    //add comment
     constructor( // Segundo constructor
         uno: Int?, // Parametros
         dos: Int // Parametros
