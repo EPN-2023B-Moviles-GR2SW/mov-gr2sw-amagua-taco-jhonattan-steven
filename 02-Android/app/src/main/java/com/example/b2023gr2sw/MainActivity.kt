76166b85b3f4636e5e8632551bf1fcff5a04a47a
Jhonattan Amagua <jhonattan.amagua@epn.edu.ec>
2023-12-05 09:54:47 -0500

    Android class Activity AppMobile Gr2sw 15-05-2023: Added: *color colorText *color colorBack *string holaMundo

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.b2023gr2sw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.AppOpsManagerCompat
 import com.example.b2023gr2sw.ui.theme.B2023gr2swTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         setContent {
             B2023gr2swTheme {
                 // A surface container using the 'background' color from the theme
