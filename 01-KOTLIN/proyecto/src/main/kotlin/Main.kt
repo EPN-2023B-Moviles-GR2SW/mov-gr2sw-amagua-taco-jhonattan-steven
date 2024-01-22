00e032ac75a4d65cde71b1f0c226b25511cbfd98
Jhonattan Amagua <jhonattan.amagua@epn.edu.ec>
2024-01-21 21:29:36 -0500

    intelli git

--- 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01-KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -46,10 +46,18 @@ fun main(args: Array<String>) {
     calcularSueldo (10.00, 15.00, 20.00)
     calcularSueldo (10.00, bonoEspecial = 20.00) // Named Parameters
     calcularSueldo (bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) // Parametros
-
+//para instanciar ya no es necesario la palabra new
     val sumaUno = Suma (1, 1)
     val sumaDos = Suma (null, 1)
     val sumaTres = Suma (1, null)
+    val sumaCuatro =Suma(null,null)
+    sumaUno.sumar()
+    sumaDos.sumar()
+    sumaTres.sumar()
+    sumaCuatro.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
@@ -88,12 +96,11 @@ class Suma( // Constructor Primario Suma
     unoParametro: Int, // Parametro
     dosParametro: Int, // Parametro
     ): Numeros (unoParametro, dosParametro) { // Extendiendo y mandando los parametros (super)
+        //: ConstructorClasePadre (Parametro1,parametro2)
     init { // Bloque codigo constructor primario
         this.numeroUno
         this.numeroDos
     }
-    //add comment
-    //commit and push
     constructor( // Segundo constructor
         uno: Int?, // Parametros
         dos: Int // Parametros
@@ -108,6 +115,33 @@ class Suma( // Constructor Primario Suma
         uno,
         if(dos == null) 0 else dos,
     )
+    //cuarto constructot
+    constructor(uno: Int?, dos: Int?): this(
+        if(uno==null) 0 else uno,
+        if(dos==null) 0 else uno,
+    )
+    fun sumar(): Int{
+        val total = numeroUno+ numeroDos
+        //Suma.agregarHistorial(total) Cuando se esta fuera de la clase es necesario especificar de que clase es
+        agregarHistorial(total)
+        return total
+    }
+    //En kotlin tanto metodos como propiedades son por defecto publico
+    //static
+    // Un metodo statico era para usar sin implementar una instancia
+    //Singleto es que solo exista una unica instancia de la clase
+    //en Kotlin es posible realizar en el companion object declarar atributos y metodos compartidos en todas las instancias de la clase
+    companion object{
+        val pi=3.14
+        fun elevarAlCuadrado(num: Int): Int{
+            return num*num
+        }
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorNuevaSuma: Int){
+            historialSumas.add(valorNuevaSuma)
+        }
+    }
+
 }
 //void -> Unit
 fun imprimirNombre(nombre: String): Unit{
