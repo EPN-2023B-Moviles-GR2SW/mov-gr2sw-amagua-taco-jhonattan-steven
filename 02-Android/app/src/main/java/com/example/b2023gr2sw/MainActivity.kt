16f04bc93c658425fef497a4a772adfee5f44044
Jhonattan Amagua <jhonattan.amagua@epn.edu.ec>
2024-02-27 17:39:35 -0500

    Solucion Ejecutar App, fue necesario corregir el theme en el manifest quitando el theme tanto para la activity como para la config remplazandolo por: android:theme="@style/Theme.AppCompat.Light"

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -18,32 +18,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContent {
-            B2023gr2swTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    B2023gr2swTheme {
-        Greeting("Android")
     }
 }
