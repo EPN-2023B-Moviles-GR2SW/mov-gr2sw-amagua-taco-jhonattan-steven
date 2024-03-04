e7a3c6504c77050d9e041bd4dd231295774eadb1
Jhonattan Amagua <jhonattan.amagua@epn.edu.ec>
2024-03-03 20:29:39 -0500

    Snack bar con dos funciones nuevas onSave instance state, guardar la variable del snackbar, onRestore permite que cuando se cambia a landscape se recupere la variable guardada con la key asignada en este caso TEXTOGuardado, y el valor de la variable en este caso textoGlobal, y al recuperarlo este se muestra en el snack bar y el texto recuperado se asigna al texto global

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
@@ -2,6 +2,7 @@ package com.example.b2023gr2sw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -19,7 +20,7 @@ class ACicloVida : AppCompatActivity() {
     fun mostrarSnackbar(texto:String){
         textoGlobal +=texto
         val snack = Snackbar.make(findViewById(R.id.cl_ciclo_vida),
-            textoGlobal, Snackbar.LENGTH_LONG)
+            textoGlobal, Snackbar.LENGTH_INDEFINITE)
         snack.show()
     }
     override fun onStart() {
@@ -45,5 +46,26 @@ class ACicloVida : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mostrarSnackbar("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            //Guardar las variables
+            // primitivas (int, string, var, etc)
+            putString("TEXTOGuardado", textoGlobal)
+
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val textoRecuperado: String? = savedInstanceState.getString("TEXTOGuardado")
+        if(textoRecuperado != null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
     }
 }
